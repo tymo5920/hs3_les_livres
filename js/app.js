7fc92290a9d1b68bd8ebee259e9861491990ce4f
KGrzeg <kupczykgrzeg@gmail.com>
2023-02-26 03:06:56 +0100

    Render shelves

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 const API = (() => {
   const BASE_PATH = "https://inventaire.io/api";
-  const USER_ID = "6a65aa87e68e161650bf7741ad001249"; //hs3 account
+  const USER_ID = "9427574a67c4e8b2ecd6e3bfe315efbf"; //hs3 account
 
   const myFetch = (url, ...args) => {
     return fetch(url, {
@@ -19,11 +19,35 @@ const API = (() => {
   }
 })()
 
+const renderShelf = (shelf) => {
+  const { name, _id } = shelf;
+
+  const template = document.querySelector("template#tpl-shelf");
+  const clone = template.content.cloneNode(true);
+  clone.querySelector(".tpl-name").innerText = name;
+  clone.querySelector(".tpl-content").id = `shelf-${_id}`;
+
+  return clone;
+}
+
+const loadShelves = async () => {
+  const target = document.querySelector("#target-books");
+  const shelves = (await API.listShelves()).shelves
+
+  for (const shelfID in shelves) {
+    const shelf = shelves[shelfID];
+    const shelfNode = renderShelf(shelf);
+    target.appendChild(shelfNode);
+  }
+
+  return shelves;
+}
+
 document.addEventListener("DOMContentLoaded", async () => {
   if (!('content' in document.createElement('template'))) {
     alert("You need to update your browser in order to view this page!")
   }
 
-  console.log(await API.listShelves())
+  const shelves = await loadShelves();
 
 })
